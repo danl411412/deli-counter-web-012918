b3839ba6ec24216c52ebfb81eba82f6879544d49
danl411412 <daan.b.lee@gmail.com>
2018-01-24 06:28:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,15 @@
 def line(arr)
   if arr.length == 0
     puts "The line is currently empty."
+  else
+    current_line = "The line is currently: "
+    line_position = 1
+    arr.each do |name|
+      current_line += "#{line_position}. #{name}"
+      if line_position < arr.size
+        current_line += " "
+      end
+    end
+    current_line
   end
-
 end
